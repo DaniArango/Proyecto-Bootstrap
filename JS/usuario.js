f91b9f82836c6dbc70d418ec5e353987cd308d5e
DaniArango <daniarangop93@gmail.com>
2022-10-09 19:50:15 +0200

    acutalizacion de js

--- JS/usuario.js
+++ JS/usuario.js
@@ -1,3 +1,5 @@
+
+
 const primerNombre = document.getElementById("nombre");
 const apellido = document.getElementById("apellido");
 const email = document.getElementById("email");
@@ -16,18 +18,33 @@ function onSubmitF(e) {
         password:password.value,}
 
       if (primerNombre.value === "" || apellido.value === "" || email.value === "" || password.value === "" || password2.value === "" ) {
-        msg.innerHTML = "<p class='tex-completo'> Lee con atención y rellena el formulario</p>";
+       //.innerHTML = "<p class='tex-completo'> Lee con atención y rellena el formulario</p>";
+       alert("Lee con atención y rellena el formulario")
       } 
-      else if (/^\ w+@ [a-zA-Z_]+?\.[a-zA-Z] {2,3}$/.test(email.value) == false) {
-        msg.innerHTML = "El email ingresado no es correcto";
+      else if (/^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$/.test(email.value) == false) {
+       // msg.innerHTML = "El email ingresado no es correcto";
+       alert("El email ingresado no es correcto")
       } 
-      else if (/^(?=[^\d_].*?\d)\w(\w|[ !@ #$%]){7,20}/.test(password.value && password2.value) == false) {
-        msg.innerHTML = "<p class='tex-contraseña'> La cotraseña no cumple los requerimientos</p>"
+      else if (/^(?=.*[0-9]+.*)(?=.*[a-zA-Z]+.*)[0-9a-zA-Z]{6,}$/.test(password.value && password2.value) == false) {
+        //msg.innerHTML = "<p class='tex-contraseña'> La cotraseña no cumple los requerimientos</p>"
+        alert(" La cotraseña no cumple los requerimientos")
       } 
       else {
-        msg.innerHTML = "<p class='tex-datos'> ¡Aww yeah! Ahora eres parte de la mejor comunidad Tomb Raider...¡¡¡FELICIDADES!!!</p>";
+        //msg.innerHTML = "<p class='tex-datos'> ¡Aww yeah! Ahora eres parte de la mejor comunidad Tomb Raider...¡¡¡FELICIDADES!!!</p>";
+        alert("Aww yeah! Ahora eres parte de la mejor comunidad Tomb Raider...¡¡¡FELICIDADES!!!")
       }
       localStorage.setItem("form", JSON.stringify(usuario));
+      paintUser()
     }
 
+
+    function paintUser(){
+      const userStorage =JSON.parse(localStorage.getItem('userStorage'));
+      console.log(userStorage)
+      
+  }
+
+  boton.addEventListener('click',onSubmitF)
+    
+ 
     
