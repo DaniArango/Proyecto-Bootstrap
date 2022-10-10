72b2b47ad809982a7cfca77cf847b8135debe183
DaniArango <daniarangop93@gmail.com>
2022-10-10 04:19:05 +0200

    cansada de la vida, cambios en js para crear cards y alerts, no me gusta bootstrap

--- JS/getusuarios.js
+++ JS/getusuarios.js
@@ -8,10 +8,14 @@ for(let i=0; i<users.length; i++) {
   }
 
   
-  function generateListItems(arg){
+  /*function generateListItems(arg){
     let allusers=" "
     for(let i=0; i<arg.length; i++) {
-        allusers +=  `<li>${arg[i].primerNombre}</li>  `   }
+        allusers +=  `<li>${arg[i].primerNombre}</li>  `
+        allusers +=  `<li>${arg[i].apellido}</li>  ` 
+        allusers +=  `<li>${arg[i].email}</li>  `   
+    }
+        
     return allusers
   }
 
@@ -19,7 +23,33 @@ for(let i=0; i<users.length; i++) {
   <ol>
   ${generateListItems(users)}
   </ol>
-  `;
+  `;*/
+
+  function ListaUsuarios() {
+   
+    while (usuariost.firstChild) {
+      usuariost.removeChild(usuariost.firstChild);
+    }
+  
+    for(let i = 0; i < users.length; i++) {
+        const div_card = document.createElement('div');
+        div_card.className = "card"
+        div_card.innerHTML = 
+        `       <div class="card-header">
+                    Tomb Raider${i + 1}
+                </div>
+                <ul class="list-group list-group-flush">
+                  <li class="list-group-item"><div><b>Nombre:</b></div> <div>${users[i].primerNombre}</div></li>
+                  <li class="list-group-item"><div><b>Nombre:</b></div> <div>${users[i].apellido}</div></li>
+                  <li class="list-group-item"><div><b>Correo:</b></div> <div>${users[i].email}</div></li>
+                </ul>
+               `;
+    
+        usuariost.appendChild(div_card);
+    }
+  }
+
+ListaUsuarios(); 
 
 
 
--- JS/usuario.js
+++ JS/usuario.js
@@ -1,5 +1,4 @@
 
-
 const primerNombre = document.getElementById("nombre");
 const apellido = document.getElementById("apellido");
 const email = document.getElementById("email");
@@ -7,6 +6,8 @@ const password = document.getElementById("password");
 const password2 = document.getElementById("password2");
 const boton = document.getElementById("botn1")
 const boton2 = document.getElementById("botn2")
+
+const div_sucess = document.querySelector('.alert-success');
 
 
 function paintUser(){
@@ -42,18 +43,21 @@ function onSubmitF(e) {
       } 
       else if (/^(?=.*[0-9]+.*)(?=.*[a-zA-Z]+.*)[0-9a-zA-Z]{6,}$/.test(password.value && password2.value) == false) {
         //msg.innerHTML = "<p class='tex-contraseña'> La cotraseña no cumple los requerimientos</p>"
-        alert(" La cotraseña no cumple los requerimientos")
+        alert(" La contraseña no cumple los requerimientos")
       } 
       else {
-        //msg.innerHTML = "<p class='tex-datos'> ¡Aww yeah! Ahora eres parte de la mejor comunidad Tomb Raider...¡¡¡FELICIDADES!!!</p>";
-        alert("Aww yeah! Ahora eres parte de la mejor comunidad Tomb Raider...¡¡¡FELICIDADES!!!")
+              //msg.innerHTML = "<p class='tex-datos'> ¡Aww yeah! Ahora eres parte de la mejor comunidad Tomb Raider...¡¡¡FELICIDADES!!!</p>";
+              alert("Aww yeah! Ahora eres parte de la mejor comunidad Tomb Raider...¡¡¡FELICIDADES!!!")
 
-        const usuarios = JSON.parse(localStorage.getItem("users"));
-        let usuariostomb= usuarios || [];
-        usuariostomb.push(usuario)
-        localStorage.setItem("users", JSON.stringify(usuariostomb))
-        paintUser();
-    
+              const usuarios = JSON.parse(localStorage.getItem("users"));
+              let usuariostomb= usuarios || [];
+              usuariostomb.push(usuario)
+              localStorage.setItem("users", JSON.stringify(usuariostomb))
+              paintUser();
+              Alertsucess();
+              
+             
+          
        
       }
 
@@ -63,7 +67,7 @@ function onSubmitF(e) {
     password.value =""
     password2.value= ""
     setTimeout(function () {
-    }, 3000);
+    }, 5000);
 
 
     }
@@ -71,6 +75,24 @@ function onSubmitF(e) {
     function onDelete(e)
     {
       localStorage.clear()
+    }
+
+    function Alertsucess(){
+
+        while (div_sucess.firstChild) {
+          div_sucess.removeChild(div_sucess.firstChild);
+      }
+        const pop_alert = document.createElement('div');
+        pop_alert.className = "alert alert-success mt-3";
+        pop_alert.role = "alert";
+        pop_alert.innerHTML = "Bienvenid@ a la mejor comunidad ¡¡FELICIDADES!!";
+        div_sucess.appendChild(pop_alert);
+        
+        
+        setTimeout(() => {
+            pop_alert.remove();
+            location.href = "formulario.html"
+        }, 5000);
     }
 
   boton.addEventListener('click',onSubmitF)
